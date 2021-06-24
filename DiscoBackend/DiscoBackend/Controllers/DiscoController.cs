0bc12db037150a01dd549fcc467fc47597c5b16f
Mikael Stalvik <mikael.stalvik@gmail.com>
2021-06-24 10:48:50 +0200

    Initial backend commit

---
+++ DiscoBackend/DiscoBackend/Controllers/DiscoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DiscoBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiscoController : ControllerBase
+    {
+        private readonly ILogger<DiscoController> _logger;
+
+        public DiscoController(ILogger<DiscoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Player> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Player
+                {
+                    Name = "Player name",
+                    Hole = rng.Next(20),
+                    Score = rng.Next(9)
+                })
+                .ToArray();
+        }
+    }
+}
---
+++ DiscoBackend/DiscoBackend/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscoBackend
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int Hole { get; set; }
+    }
+}
